8baf961e41da4818103fe9fba29daac4b8b1e1f4
김성은 <kse012050@naver.com>
2020-01-28 07:58:54 +0900

    sanEnd PC버전 퍼블리싱 임시 완료했다

--- js/common.js
+++ js/common.js
@@ -1,10 +1,14 @@
-var menuColorboolean = true;
+
     var mainMobileMainAreaH;
     $(document).ready(function(){
         mainImgChange();
         mainVisionClick();
         menuClick();
         scrollEvent();
+
+        $(".wrapArea header h1").click(function(){
+            $("section").removeClass("scrollClass");
+        })
 
 
         $(window).resize(function(){
@@ -32,6 +36,7 @@ var menuColorboolean = true;
 
     function scrollEvent(){
         var scrollClass;
+        var pageScrollClass = 1;
         $('section').each(function(){
             // section의 갯수 만큼 네비게이션 추가
             $("header ul").append("<li></li>");
@@ -40,66 +45,67 @@ var menuColorboolean = true;
                 $(this).on("DOMMouseScroll mousewheel", function (event) {
                     scrollClass = $(".scrollClass");
                     mouseWheel = event.originalEvent.wheelDelta;
+                    // 스크롤 이벤트 반응형
                     if($(window).width() >= 768){
+                        // 마우스 휠을 위로 올렸을 때
                         if(mouseWheel > 0){
-                            if(($(this).prev()[0].localName) == "section"){
+                            if($(this).children().hasClass("test")){
+                                $(".test").eq($(".test").length - 1 ).removeClass("test");
+                                pageScrollClass = 1;
+                            }else if(($(this).prev()[0].localName) == "section"){
                                 if(parseInt($("section").eq(scrollClass.length -1 ).css("bottom")) == $(window).height() && parseInt($("section").eq(scrollClass.length).css("bottom")) == 0){
                                     $(this).prev().removeClass("scrollClass");
                                 }
                             }
+                        // 마우스 휠을 아래로 내렸을 때
                         }else if(mouseWheel < 0 ){
-                            if($(this).next()[0].localName == "section"){
+                            // console.log($(this).children(".test").length);
+                            // console.log($(this).children(".test").eq($(this).children(".test").length -1).css("top"));
+                            
+                            if($(this).children().hasClass("pageArea") && $(this).children(".pageArea").length > pageScrollClass ){
+                                // console.log('$(this).children().hasClass("pageArea") : ' + $(this).children().hasClass("pageArea"));
+                                // console.log(' $(this).children(".pageArea").length : ' + $(this).children(".pageArea").length);
+                                // console.log('pageScrollClass : ' + pageScrollClass);
+                                // console.log('$(this).children(".test").eq(0).height() : ' + $(this).children(".test").eq(0).height());
+                                if($(this).children(".test").eq(0).height() == undefined || $(this).children(".test").eq(pageScrollClass -2).css("top") == "0px"){
+                                    $(this).children().eq(pageScrollClass).addClass("test");
+                                    pageScrollClass = $(this).children(".test").length + 1;
+                                }
+                            }
+                            else if($(this).children(".test").eq($(this).children(".test").length -1).css("top") != "0px" && $(this).children(".test").eq($(this).children(".test").length -1).css("top") != undefined){
+                                // alert();
+                            }else if($(this).next()[0].localName == "section"){
                                 if($(this)[0].className == $('section').eq(0)[0].className || parseInt(scrollClass.eq(scrollClass.length -1).css("bottom")) == $(window).height()){
                                         $(this).addClass("scrollClass");
+                                        pageScrollClass = 1;
                                 }
                             }
                         }
-                            
-                            if(scrollClass.length > 0 && scrollClass.eq(scrollClass.length -1).next()[0].className =="aboutArea"){
+
+
+                        scrollClass = $(".scrollClass");
+                        // 메인 aboutArea 에 왔을 때, (로고 , 메뉴버튼 , 네비게이션) 색상 변경
+                        if(scrollClass.length > 0 && scrollClass.eq(scrollClass.length -1).next()[0].className =="aboutArea"){
+                            $("header").addClass("headerBlack");
+                        }else{
+                            $("header").removeClass("headerBlack");
+                        }
+
+                        // 네비게이션 위치
+                        $("header ul li").removeClass("active");
+                        $("header ul li").eq(scrollClass.length).addClass("active");
+
+               
+                    }else{
+                        
+                        
+                        mainMobileMainAreaH = parseInt($(".mainArea").css("height").replace(/[^0-9]/g,""));
+                       
+                        setTimeout(function(){
+                            if(mainMobileMainAreaH < $(window).scrollTop() +30){
                                 $("header").addClass("headerBlack");
                             }else{
                                 $("header").removeClass("headerBlack");
-                            }
-
-               
-
-                        if($("header h1").hasClass("logoBlack")){
-                            menuColorboolean = false;
-                        }else{
-                            menuColorboolean = true;
-                        }
-                    }else{
-                        
-                        // console.log(scrollPercent);
-                        // var mainMobileH1Top = $("h1").offset().top;
-                        // console.log(mainMobileH1Top);
-                        // var mainMobileMainAreaH = parseInt($(".mainArea").css("height").replace(/[^0-9]/g,""));
-                        // console.log("메인" + (mainMobileMainAreaH -35));
-                        // if(mainMobileH1Top >= mainMobileMainAreaH -50){
-                        //     $("header h1").addClass("logoBlack");
-                        // }else{
-                        //     $("header h1").removeClass("logoBlack");
-                        // }
-                        // if(scrollPercent > 35){
-                        //     $("header h1").addClass("logoBlack");
-                        // }else{
-                        //     $("header h1").removeClass("logoBlack");
-                        // }
-                        mainMobileMainAreaH = parseInt($(".mainArea").css("height").replace(/[^0-9]/g,""));
-                        // // console.log($(window).scrollTop());
-                        // console.log(scrollPercent);
-                        // if(mouseWheel > 0){
-                          
-                        // }else if(mouseWheel < 0 && $(window).scrollTop() > (mainMobileMainAreaH - 100)){
-                        //     $("header h1").addClass("logoBlack");
-                        // }
-                        setTimeout(function(){
-                            // console.log("0 : " + $(window).scrollTop());
-                            // console.log("1 : " + mainMobileMainAreaH);
-                            if(mainMobileMainAreaH < $(window).scrollTop() +30){
-                                $("header h1").addClass("logoBlack");
-                            }else{
-                                $("header h1").removeClass("logoBlack");
                             }
                         },250);
                     }
@@ -115,9 +121,6 @@ var menuColorboolean = true;
         var mainImgArr = $(".mainArea .mainImgChangeArea li");
         var mainImgArrNum = 1;
         $(".mainArea div span:last-of-type").text("0"+ (mainImgArr.length));
-
-    
-
          setInterval(function(){
              mainImgArr.removeClass("showOpacity")
              mainImgArr.eq(mainImgArrNum).addClass("showOpacity");
@@ -144,12 +147,16 @@ var menuColorboolean = true;
     };
 
     function menuClick(){
+        var menuColorBoolean = true;
         $("header div").on("click",function(){
            $("nav").toggleClass("active");
 
-            if(!menuColorboolean){
-                $("header h1").toggleClass("logoBlack");
-                $("header div span").toggleClass("menuBottonBlack");
+           if($("header").hasClass("headerBlack")){
+               $("header").removeClass("headerBlack");
+               menuColorBoolean = false;
+           }else if(!menuColorBoolean){
+                $("header").addClass("headerBlack");
+                menuColorBoolean = true;
             }
         });
     }
